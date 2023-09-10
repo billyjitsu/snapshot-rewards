280cce5ca6292b40ed3293e5a84e8162e7d4e218
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-09-10 15:50:17 -0700

    Initial commit

---
+++ contracts/TestLensApiConsumerContract.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./PhatRollupAnchor.sol";
+
+interface IERC20 {
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
+contract TestLensApiConsumerContract is PhatRollupAnchor, Ownable {
+    event ResponseReceived(uint reqId, string pair, uint256 value);
+    event ErrorReceived(uint reqId, string pair, uint256 errno);
+    event Deposited(address indexed user, uint256 amount);
+    event Distributed(address indexed user, uint256 totalAmount);
+
+    IERC20 public token;  // ERC20 token contract
+    uint constant TYPE_RESPONSE = 0;
+    uint constant TYPE_ERROR = 2;
+
+    mapping (string => address) public walletRegistry;
+    mapping(uint => string) requests;
+    uint nextRequest = 1;
+
+
+    constructor(address phatAttestor) {
+        _grantRole(PhatRollupAnchor.ATTESTOR_ROLE, phatAttestor);
+    }
+
+    function setAttestor(address phatAttestor) public {
+        _grantRole(PhatRollupAnchor.ATTESTOR_ROLE, phatAttestor);
+    }
+
+    function request(string calldata profileId) public {
+        // assemble the request
+        uint id = nextRequest;
+        requests[id] = profileId;
+        _pushMessage(abi.encode(id, profileId));
+        nextRequest += 1;
+    }
+
+    // For test
+    function malformedRequest(bytes calldata malformedData) public {
+        uint id = nextRequest;
+        requests[id] = "malformed_req";
+        _pushMessage(malformedData);
+        nextRequest += 1;
+    }
+
+    function _onMessageReceived(bytes calldata action) internal override {
+        require(action.length == 32 * 3, "cannot parse action");
+        (uint respType, uint id, uint256 data) = abi.decode(
+            action,
+            (uint, uint, uint256)
+        );
+        if (respType == TYPE_RESPONSE) {
+            emit ResponseReceived(id, requests[id], data);
+            delete requests[id];
+        } else if (respType == TYPE_ERROR) {
+            emit ErrorReceived(id, requests[id], data);
+            delete requests[id];
+        }
+    }
+
+    function setToken(address tokenAddress) external onlyOwner {
+        token = IERC20(tokenAddress);
+    }
+
+    function registerWallet(string calldata profileId, address walletAddress) external {
+        walletRegistry[profileId] = walletAddress;
+    }
+
+    // Deposit tokens into this contract
+    function depositTokens(uint256 amount) external {
+        require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
+        emit Deposited(msg.sender, amount);
+    }
+
+    // Distribute tokens to the list of addresses
+     function distributeTokens(address[] memory recipients) external {
+        uint256 totalAmount = token.balanceOf(address(this));
+        uint256 numOfRecipients = recipients.length;
+
+        require(numOfRecipients > 0, "No recipients provided");
+        uint256 amountPerRecipient = totalAmount / numOfRecipients;
+        require(amountPerRecipient > 0, "Amount per recipient is zero");
+
+        for (uint i = 0; i < numOfRecipients; i++) {
+            require(token.transfer(recipients[i], amountPerRecipient), "Transfer failed");
+        }
+        emit Distributed(msg.sender, totalAmount);
+    }
+
+    //
+    function getAddress(string memory key) public view returns (address) {
+        return walletRegistry[key];
+    }
+
+}
---
+++ src/query.ts
@@ -0,0 +1,42 @@
+import { GraphQLClient, gql } from 'graphql-request';
+
+async function fetchVotes() {
+
+    function flattenVoterArray(obj) {
+        if (!obj.votes) {
+          return [];
+        }
+      
+        return obj.votes.map(vote => vote.voter);
+      }
+  const endpoint = 'https://hub.snapshot.org/graphql';
+  const client = new GraphQLClient(endpoint);
+
+  const query = gql`
+    query {
+      votes(
+        first: 10
+        skip: 0
+        where: {
+          proposal: "QmPvbwguLfcVryzBRrbY4Pb9bCtxURagdv1XjhtFLf3wHj"
+        }
+        orderBy: "created"
+        orderDirection: desc
+      ) {        
+        voter   
+      }
+    }
+  `;
+
+  try {
+    const data = await client.request(query);
+   //console.log(data);
+    let flattenData = flattenVoterArray(data);
+   // console.log(flattenData);
+    return flattenData;
+  } catch (error) {
+    console.error('Error:', error.response.errors);
+  }
+}
+
+export default fetchVotes;
